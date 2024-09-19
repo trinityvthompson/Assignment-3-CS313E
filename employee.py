d4dc1388be8c1593cc5100488b73b9d88934c037
trinityvthompson <trinityvthompson@utexas.edu>
2024-09-18 20:31:16 -0500

    Update employee.py

--- employee.py
+++ employee.py
@@ -3,90 +3,77 @@
 class Employee:
 
     def __init__(self, **kwargs):
-    # Add your code here!
+        self.name = kwargs.get("name", "None")
+        self.identifier = kwargs.get("identifier", "None")
+        self.salary = kwargs.get("salary", "None") # replace not provided later
 
     def __str__(self):
-    # Add your code here!
+        return (f"Employee\n{self.name}, {self.identifier}, {self.salary}")
 
 
-############################################################
-############################################################
-############################################################
-
-class PermanentEmployee:
+class PermanentEmployee(Employee):
 
     def __init__(self, **kwargs):
-
+        super().__init__(self, **kwargs)
+        self.benefits = kwargs.get("benefits", [])
 
     def cal_salary(self):
-
-
-
-    def __str__(self):
-
-
-############################################################
-############################################################
-############################################################
-
-class Manager :
-
-    
-    def cal_salary(self):
-
+        if "health_insurance" in self.benefits and "retirement" in self.benefits:
+            return self.salary * 0.7
+        elif "health_insurance" in self.benefits:
+            return self.salary * 0.9
+        elif "retirement" in self.benefits:
+            return self.salary * 0.8
 
     def __str__(self):
+        return (f"PermanentEmployee\n{self.name}, {self.identifier}, {self.salary}, {self.benefits}")
 
+class Manager(Employee):
 
-
-############################################################
-############################################################
-############################################################
-class TemporaryEmployee:
     def __init__(self, **kwargs):
-        
+        super().__init__(self, **kwargs)
+        self.bonus = kwargs.get("bonus", 0)
     
     def cal_salary(self):
-        
+        return self.salary + self.bonus
 
     def __str__(self):
-        
+        return (f"Manager\n{self.name}, {self.identifier}, {self.salary}, {self.bonus}")
 
+
+class TemporaryEmployee(Employee):
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self.hours = kwargs.get("hours", 0)
     
-############################################################
-############################################################
-############################################################
+    def cal_salary(self):
+        return self.salary * self.hours
+
+    def __str__(self):
+        return (f"TemporaryEmployee\n{self.name}, {self.identifier}, {self.salary}, {self.hours}")   
 
 
-class Consultant :
-  
+class Consultant(TemporaryEmployee):
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self.travel = kwargs.get("travel", 0)
 
     def cal_salary(self):
-  
+        return super().cal_salary() + (1000 * self.travel)
 
     def __str__(self):
-  
-############################################################
-############################################################
-############################################################
+        return (f"Consultant\n{self.name}, {self.identifier}, {self.salary}, {self.hours}, {self.travel}")
 
 
-class ConsultantManager:
+class ConsultantManager(Consultant, Manager):
     def __init__(self,  **kwargs):
- 
+        super().__init__(self, **kwargs)
 
     def cal_salary(self):
- 
+        return (self.salary * self.hours) + (self.travel * 1000) + self.bonus
+
     def __str__(self):
- 
-
-
-############################################################
-############################################################
-############################################################
-
-
-
+        return (f"ConsultantManager\n{self.name}, {self.identifier}, {self.salary}, {self.hours}, {self.bonus}, {self.travel}")
 
 
 ###### DO NOT CHANGE THE MAIN FUNCTION ########
